4094f7fb20849ecd12f6947a8875afc3930c9f06
josebauer <jose.h.bauer03@gmail.com>
2025-05-19 15:32:52 -0300

    Ajusta Classe Usuario e funções / Esconde senha com getpass

--- sistema.py
+++ sistema.py
@@ -1,4 +1,5 @@
 from usuario import Usuario
+import getpass
 
 usuarios = []
 
@@ -12,22 +13,22 @@ def cadastrar_usuario():
   nomeUsuario = input('\nDigite o nome completo: ')
   cpf = int(input('Digite o CPF: '))
   email = input('Digite o email: ')
-  senha = input('Insira uma senha forte: ')
+  senha = getpass.getpass('Insira uma senha forte: ')
   funcao = input('Digite a função (Atendente / Administrador): ')
   
   usuario = Usuario(nomeUsuario, cpf, email, senha, funcao)
   usuarios.append(usuario)
   
-  return '\nUsuário cadastrado com sucesso!'
+  print('\nUsuário cadastrado com sucesso!')
 
 def listar_usuarios():
   if not usuarios:
-    return '\nNão há usuários cadastrados.'
+    print('\nNão há usuários cadastrados.')
   else:
     print('\n----- Usuários Cadastrados -------')
     for usuario in usuarios:
-      print(f'\nNome: {usuario._nome}')
-      print(f'\nFunção: {usuario._funcao}')
+      print(f'\nNome: {usuario.nome}')
+      print(f'\nFunção: {usuario.funcao}')
       print('\n----------------------------------')
           
 def consultar_usuario():
@@ -38,7 +39,7 @@ def consultar_usuario():
     encontrado = False
     
     for usuario in usuarios:
-      if usuario._cpf == pesquisa_cpf:
+      if usuario.cpf == pesquisa_cpf:
         usuario.ver_dados()
         encontrado = True
     if not encontrado:
@@ -51,8 +52,45 @@ def editar_usuario():
 
   cpf = int(input('\nDigite o CPF do usuário que deseja editar: '))
   for usuario in usuarios:
-    if usuario._cpf == cpf:
-      print('\n--- Editar dados do usuário ---')
+    if usuario.cpf == cpf:
+      print('\n--- Editar dados do usuário ---\n')
+      print('1 - Nome')
+      print('2 - CPF')
+      print('3 - Email')
+      print('4 - Senha')
+      print('5 - Função')
+      print('0 - Cancelar')
+      opcao = input('\nEscolha a opção que deseja editar: ')
+
+      if opcao == '1':
+        usuario.nome = input('Novo nome: ')
+        print('Nome atualizado com sucesso.')
+      elif opcao == '2':
+        usuario.cpf = int(input('Novo CPF: '))
+        print('CPF atualizado com sucesso.')
+      elif opcao == '3':
+        usuario.email = input('Novo email: ')
+        print('Email atualizado com sucesso.')
+      elif opcao == '4':
+        usuario.senha = getpass.getpass('Nova senha: ')
+        print('Senha atualizada com sucesso.')
+      elif opcao == '5':
+        usuario.funcao = input('Nova função (Atendente / Administrador): ')
+        print('Função atualizada com sucesso.')
+      elif opcao == '0':
+        print('Edição cancelada.')
+      else:
+        print('Opção inválida.')
+
+      return
+  print('\nUsuário com esse CPF não foi encontrado.')
+  if not usuarios:
+    print('\nNão há usuários cadastrados.')
+
+  cpf = int(input('\nDigite o CPF do usuário que deseja editar: '))
+  for usuario in usuarios:
+    if usuario.cpf == cpf:
+      print('\n--- Editar dados do usuário ---\n')
       print('1 - Nome')
       print('2 - CPF')
       print('3 - Email')
@@ -63,28 +101,28 @@ def editar_usuario():
 
       if opcao == '1':
         novo_nome = input('Novo nome: ')
-        usuario._nome = novo_nome
-        print('Nome atualizado com sucesso.')
+        usuario.nome = novo_nome
+        print('\nNome atualizado com sucesso.')
       elif opcao == '2':
         novo_cpf = input('Novo CPF: ')
-        usuario._cpf = novo_cpf
-        print('CPF atualizado com sucesso.')
+        usuario.cpf = novo_cpf
+        print('\nCPF atualizado com sucesso.')
       elif opcao == '3':
         novo_email = input('Novo email: ')
-        usuario._email = novo_email
-        print('Email atualizado com sucesso.')
+        usuario.email = novo_email
+        print('\nEmail atualizado com sucesso.')
       elif opcao == '4':
-        nova_senha = input('Nova senha: ')
-        usuario._senha = nova_senha
-        print('Senha atualizada com sucesso.')
+        nova_senha = getpass.getpass('Nova senha: ')
+        usuario.senha = nova_senha
+        print('\nSenha atualizada com sucesso.')
       elif opcao == '5':
         nova_funcao = input('Nova função (Atendente / Administrador): ')
-        usuario._funcao = nova_funcao
-        print('Função atualizada com sucesso.')
+        usuario.funcao = nova_funcao
+        print('\nFunção atualizada com sucesso.')
       elif opcao == '0':
-        print('Edição cancelada.')
+        print('\nEdição cancelada.')
       else:
-        print('Opção inválida.')
+        print('\nOpção inválida.')
 
       return
   print('\nUsuário com esse CPF não foi encontrado.')
@@ -93,12 +131,12 @@ while logado == False:
   print('\n--------- Login ------------\n')
   
   email = input('Digite o seu email: ')
-  senha = input('Digite a sua senha: ')
+  senha = getpass.getpass('Digite a sua senha: ')
   
   encontrado = False 
 
   for usuario in usuarios:
-    if usuario._email == email and usuario._senha == senha:
+    if usuario.email == email and usuario.senha == senha:
       print('\nLogin realizado com sucesso!')
       logado = True
       encontrado = True
@@ -109,12 +147,12 @@ while logado == False:
   
 if logado == True:
   while True:
-    print('\n--- Gerenciamento de Usuários ---')
-    print('\n1 - Cadastrar novo usuário')
-    print('\n2 - Ver usuários cadastrados')
-    print('\n3 - Consultar pelo CPF')
-    print('\n4 - Editar dados usuário')
-    print('\n0 - Sair')
+    print('\n--- Gerenciamento de Usuários ---\n')
+    print('1 - Cadastrar novo usuário')
+    print('2 - Ver usuários cadastrados')
+    print('3 - Consultar pelo CPF')
+    print('4 - Editar dados usuário')
+    print('0 - Sair')
     print('\n---------------------------------')
     
     opcao = int(input('\nEscolha uma opção: '))
--- usuario.py
+++ usuario.py
@@ -6,45 +6,45 @@ class Usuario:
     self._senha = senha
     self._funcao = funcao # Atendente / Administrador
     
-    @property
-    def nome(self):
-      return self._nome
+  @property
+  def nome(self):
+    return self._nome
 
-    @nome.setter
-    def nome(self, novo_nome):
-      self._nome = novo_nome
-    
-    @property
-    def cpf(self):
-      return self._cpf
-    
-    @cpf.setter
-    def cpf(self, novo_cpf):
-      self._cpf = novo_cpf
-    
-    @property
-    def email(self):
-      return self._email
-    
-    @email.setter
-    def email(self, novo_email):
-      self._email = novo_email
-    
-    @property
-    def senha(self):
-      return self._senha
+  @nome.setter
+  def nome(self, novo_nome):
+    self._nome = novo_nome
+  
+  @property
+  def cpf(self):
+    return self._cpf
+  
+  @cpf.setter
+  def cpf(self, novo_cpf):
+    self._cpf = novo_cpf
+  
+  @property
+  def email(self):
+    return self._email
+  
+  @email.setter
+  def email(self, novo_email):
+    self._email = novo_email
+  
+  @property
+  def senha(self):
+    return self._senha
 
-    @senha.setter
-    def senha(self, nova_senha):
-      self._senha = nova_senha
-    
-    @property
-    def funcao(self):
-      return self._funcao
-    
-    @funcao.setter
-    def funcao(self, nova_funcao):
-      self._funcao = nova_funcao
+  @senha.setter
+  def senha(self, nova_senha):
+    self._senha = nova_senha
+  
+  @property
+  def funcao(self):
+    return self._funcao
+  
+  @funcao.setter
+  def funcao(self, nova_funcao):
+    self._funcao = nova_funcao
   
   def ver_dados(self):
     print('\n----- Dados do usuário -----')
